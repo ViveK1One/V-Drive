2ec10b40e3a889c81eef4adf89a2b2057a7aa4f5
ViveK1One <167016976+ViveK1One@users.noreply.github.com>
2024-12-01 17:50:23 +0530

    files added

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+//here we have used createSessionClient because we don't want to allow user to use data withput login in
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) throw new Error("No session");
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+//this is more powerful this one creates client instances with 
+//admin level permission to manage your entire project
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
